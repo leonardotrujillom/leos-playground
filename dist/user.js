1d47d53a66e9becdc2cf5706f231a754beff1101
Leonardo Trujillo <lt@panaton.com>
2019-07-01 15:21:55 -0600

    Modified user schema

--- dist/user.js
+++ dist/user.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose = require("mongoose");
 const uri = "mongodb+srv://leotm2010:7Rgzc9YjoPIi7MaY@leosplayground-ldzjr.mongodb.net/test?retryWrites=true&w=majority";
+
 mongoose.connect(uri, (err) => {
     if (err) {
         console.log(err.message);
@@ -10,10 +11,17 @@ mongoose.connect(uri, (err) => {
         console.log("Succesfully Connected!");
     }
 });
+
 exports.UserSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    author: { type: String, required: true }
-});
+    id: Number,
+    username: String,
+    displayName: String,
+    emailAddress: String,
+    imageUrl: String,
+    
+    isActive: Boolean
+}, {timestamps: true});
+
 const User = mongoose.model("User", exports.UserSchema);
 exports.default = User;
 //# sourceMappingURL=user.js.map
